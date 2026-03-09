6819b69f67375d68621edb9c20475203d139dc0e
Huck <45248261+huckmon@users.noreply.github.com>
2026-03-09 15:15:44 +1030

    Fixed y-sorting and added npc constant movement
    - Fixed y-sorting by fixing it to level instead of characters
    - Peasants will not bounce around the level constantly and have a measure to unstick them if needed

--- Scripts/peasant_npc.gd
+++ Scripts/peasant_npc.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @onready var panic_sprite = $sprite/panic_animation_sprite
 @onready var panic_animation_timer = $panic_animation_timer
 
+@onready var stuck_check = $stuck_check
+
 @onready var sprite = $sprite
 @export var x_int: float = 1.1
 @export var y_int: float = 0.9
@@ -18,12 +20,13 @@ var walking_rotation: float = 15.0
 var speed: int = 150
 var count: float = 0.0
 
-var current_velocity
+var current_velocity: Vector2
 
 func _ready() -> void:
 	panic_sprite.visible = false	
 	var init_speed: Array = [-1.0, 1.0]
-	current_velocity = Vector2(init_speed.pick_random(),init_speed.pick_random())
+	#current_velocity = Vector2(init_speed.pick_random(),init_speed.pick_random())
+	current_velocity = Vector2(randf_range(-1.0,1.0),randf_range(-1.0,1.0))
 
 func _physics_process(_delta: float) -> void:
 	velocity = current_velocity.normalized() * speed
@@ -33,7 +36,6 @@ func _physics_process(_delta: float) -> void:
 	else:
 		sprite.rotation = 0.0
 		walk_animation_timer.stop()
-	print(velocity)
 	move_and_slide()
 	squash_movement()
 	count += 0.1
@@ -53,11 +55,34 @@ func walking_movements():
 	if walk_animation_timer.time_left != 0:
 		return
 	walk_animation_timer.start(walk_anim_timer)
-	
+
+func randomise_direction():
+	#current_velocity = Vector2(randf_range(-1.0,1.0),randf_range(-1.0,1.0))
+	#current_velocity = current_velocity * -1
+	if current_velocity[0] > 0:
+		current_velocity[0] = randf_range(-1.0,0)
+	else:
+		current_velocity[0] = randf_range(0,1.0)
+	if current_velocity[1] > 0:
+		current_velocity[1] = randf_range(-1.0,0)
+	else:
+		current_velocity[1] = randf_range(0,1.0)
+	stuck_check.start()
+
 func _on_walk_animation_timer_timeout() -> void:
 	sprite.rotation_degrees = sprite.rotation_degrees * -1
-
 
 func _on_panic_animation_timer_timeout() -> void:
 	panic_sprite.visible = !panic_sprite.visible
 	panic_animation_timer.start()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "floor_wall":
+		randomise_direction()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "floor_wall":
+		stuck_check.stop()
+
+func _on_stuck_check_timeout() -> void:
+	randomise_direction()
